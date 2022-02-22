3d3639ea29a077d15a30d719412f2033aa8d46a9
Håkon Skogen <84183241+Handere@users.noreply.github.com>
2022-02-22 21:45:48 +0100

    Add variables and constructor

--- src/main/java/KnightProblem.java
+++ src/main/java/KnightProblem.java
@@ -1,4 +1,19 @@
 public class KnightProblem {
     private int size;
+    private int startingColumn;
+    private int startingRow;
+
+    final int VACANT = 0;
+    final int OCCUPIED = 1;
+
     private int chessboard[][];
+    private int cellOccupied[][];
+
+    public KnightProblem(int size, int startingColumn, int startingRow) {
+        this.size = size;
+        this.startingColumn = startingColumn;
+        this.startingRow = startingRow;
+        chessboard = new int[size][size];
+    }
+
 }
